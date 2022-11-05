26343faf34642a073a687127cc1fb7562f7fa3f8
AnorakDWise <gregory442005@gmail.com>
2022-11-05 22:43:39 +0100

    init

---
+++ next-chat/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import "@fontsource/space-grotesk";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Next Chat</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Testgg</h1>
+    </div>
+  );
+}
