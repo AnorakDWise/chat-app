dad8474c9cf35c6cbe08057d6bfa277ce97d93eb
AnorakDWise <gregory442005@gmail.com>
2022-11-14 19:59:50 +0100

    idkidk

--- next-chat/pages/chat/[id].js
+++ next-chat/pages/chat/[id].js
@@ -2,6 +2,16 @@ import styled from "styled-components";
 import Head from "next/head";
 import Sidebar from "../components/Sidebar";
 import ChatScreen from "../components/ChatScreen";
+import { db } from "../../firebase";
+import {
+  collection,
+  doc,
+  orderBy,
+  getDoc,
+  getData,
+  getDocs,
+  query,
+} from "firebase/firestore";
 
 const Chat = () => {
   return (
@@ -16,6 +26,39 @@ const Chat = () => {
 };
 
 export default Chat;
+
+export async function getServerSideProps(context) {
+  const chatID = context.query.id;
+  const chatIDRef = collection(db, "chats");
+  const messageID = collection(db, "messages");
+  const ref = doc(chatIDRef, chatID);
+  const messagesRes = await getDocs(
+    query(ref, messageID, orderBy("timestamp", "asc"))
+  );
+
+  const messages = messagesRes.docs
+    .map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }))
+    .map((messages) => ({
+      ...messages,
+      timestamp: messages.timestamp.toDate().getTime(),
+    }));
+
+  const chatRes = await getData(ref);
+  const chat = {
+    id: chatRes.id,
+    ...chatRes.data(),
+  };
+
+  return {
+    props: {
+      messages: JSON.stringify(messages),
+      chat: chat,
+    },
+  };
+}
 
 const Container = styled.div`
   margin: 0 auto;
