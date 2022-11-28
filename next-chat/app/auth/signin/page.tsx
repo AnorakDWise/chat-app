7a917f130a4262b86650b014bb82af0595f3bf6a
AnorakDWise <gregory442005@gmail.com>
2022-11-28 13:46:01 +0100

    NextChat v1.0

--- next-chat/app/auth/signin/page.tsx
+++ next-chat/app/auth/signin/page.tsx
@@ -5,9 +5,12 @@ const signInPage = async () => {
   const providers = await getProviders();
   return (
     <main className="m-auto w-screen h-screen center flex-col absolute left-0 right-0 top-0 bottom-0 text-stone-200">
-      <h1 className="text-fuckingHuge font-medium md:text-bigAsUrMom">
-        NextChat
-      </h1>
+      <div className="flex justify-center items-center text-plain md:text-phoneBig">
+        <h1 className="text-fuckingHuge font-medium md:text-bigAsUrMom">
+          NextChat
+        </h1>
+        <h3>v1.0</h3>
+      </div>
       <SignInComponent providers={providers} />
     </main>
   );
